4921ff9847fa47082ea673254c5762d9350e3cae
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-03-25 10:23:49 +0000

    fix: forms validation

--- includes/blocks/login-form.php
+++ includes/blocks/login-form.php
@@ -1,87 +1,82 @@
 <?php
 
-  function mos_login_form()
-  {
-    if (is_user_logged_in()) {
-      return '';
-    }
-
-    $output = '';
-    $login_data = array();
-
-    if (isset($_POST['login-form-email']) && isset($_POST['login-form-password'])) {
-      $email = sanitize_email($_POST['login-form-email']);
-      $password = sanitize_text_field($_POST['login-form-password']);
-
-      if (empty($email)) {
-        $output .= '<div class="login-error">' . esc_html__('Email is required.', 'mos') . '</div>';
-      } else if (!is_email($email)) {
-        $output .= '<div class="login-error">' . esc_html__('Invalid email address.', 'mos') . '</div>';
-      }
-
-      if (empty($password)) {
-        $output .= '<div class="login-error">' . esc_html__('Password is required.', 'mos') . '</div>';
-      }
-
-      if (empty($output)) {
-        $login_data['user_login'] = $email;
-        $login_data['user_password'] = $password;
-        $login_data['remember'] = true;
-
-        $user = wp_signon($login_data, false);
-        if (is_wp_error($user)) {
-          $output .= '<div class="login-error">' . esc_html__('Invalid login credentials.', 'mos') . '</div>';
-        } else {
-          // Generate and store a token for the user
-          $token = wp_generate_password(32);
-          update_user_meta($user->ID, 'auth_token', $token);
-
-          // Set the token as a cookie
-          setcookie('auth_token', $token, time() + (86400 * 30), "/"); // Cookie will expire in 30 days
-          wp_set_auth_cookie($user->ID);
-          wp_safe_redirect(home_url() . '/utilizatori/comanda-noua/');
-          exit;
+    function mos_login_form() {
+        if ( is_user_logged_in() ) {
+            return '';
         }
-      }
+
+        $output     = '';
+        $login_data = array();
+
+        if ( isset( $_POST['login-form-email'] ) && isset( $_POST['login-form-password'] ) ) {
+            $email    = sanitize_email( $_POST['login-form-email'] );
+            $password = sanitize_text_field( $_POST['login-form-password'] );
+
+            if ( empty( $email ) || empty( $password ) ) {
+                $output .= '<div class="login-error">' . esc_html__( 'Invalid login credentials.', 'mos' ) . '</div>';
+            }
+
+            if ( empty( $output ) ) {
+                $login_data['user_login']    = $email;
+                $login_data['user_password'] = $password;
+                $login_data['remember']      = true;
+
+                $user = wp_signon( $login_data, false );
+                if ( is_wp_error( $user ) ) {
+                    $output .= '<div class="login-error">' . esc_html__( 'Invalid login credentials.', 'mos' ) . '</div>';
+                } else {
+                    // Generate and store a token for the user
+                    $token = wp_generate_password( 32 );
+                    update_user_meta( $user->ID, 'auth_token', $token );
+
+                    // Set the token as a cookie
+                    setcookie( 'auth_token', $token, time() + ( 86400 * 30 ), "/" ); // Cookie will expire in 30 days
+                    wp_set_auth_cookie( $user->ID );
+                    wp_safe_redirect( home_url() . '/utilizatori/comanda-noua/' );
+                    exit;
+                }
+            }
+        }
+
+
+        ob_start(); ?>
+
+        <div class="wp-block-mos-login-form">
+            <!-- sof: login form -->
+            <form id="login-form" class="login-form" method="post">
+
+                <div class="form-group">
+                    <label for="login-form-email"><?php esc_html_e( 'Email', 'mos' ); ?></label>
+                    <input type="text" id="login-form-email" name="login-form-email" class="form-input"
+                           placeholder="<?php esc_attr_e( 'Email', 'mos' ); ?>"
+                           value="<?php echo isset( $_POST['login-form-email'] ) ? esc_attr( $_POST['login-form-email'] ) : ''; ?>"/>
+                </div>
+
+                <div class="form-group">
+                    <label for="login-form-password"><?php esc_html_e( 'Password', 'mos' ); ?></label>
+                    <input type="password" id="login-form-password" name="login-form-password" class="form-input"
+                           placeholder="<?php esc_attr_e( 'Password', 'mos' ); ?>"/>
+                </div>
+
+                <div id="login-status" class="login-status" style="margin-bottom: 2rem;"><?php echo $output; ?></div>
+
+                <div class="form-footer">
+                    <div class="form-footer-meta">
+                        <?php esc_html_e( 'Don\'t have an account yet?', 'mos' ); ?>
+                        <a class="animated-link"
+                           href="<?php echo esc_url( site_url( '/utilizatori/inregistrare' ) ); ?>"><?php esc_html_e( 'Register', 'mos' ); ?></a>
+                    </div>
+                    <button class="form-subscribe-button"
+                            type="submit"><?php esc_html_e( 'Log in to your account', 'mos' ); ?></button>
+                </div>
+
+            </form>
+            <!-- eof: login form -->
+        </div>
+
+        <?php
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        return $output;
     }
-
-    ob_start(); ?>
-
-      <div class="wp-block-mos-login-form">
-          <!-- sof: login form -->
-          <form id="login-form" class="login-form" method="post">
-              <div id="login-status" class="login-status"><?php echo $output; ?></div>
-
-              <div class="form-group">
-                  <label for="login-form-email"><?php esc_html_e('Email', 'mos'); ?></label>
-                  <input type="text" id="login-form-email" name="login-form-email" class="form-input"
-                         placeholder="<?php esc_attr_e('Email', 'mos'); ?>"
-                         value="<?php echo isset($_POST['login-form-email']) ? esc_attr($_POST['login-form-email']) : ''; ?>"/>
-              </div>
-
-              <div class="form-group">
-                  <label for="login-form-password"><?php esc_html_e('Password', 'mos'); ?></label>
-                  <input type="password" id="login-form-password" name="login-form-password" class="form-input"
-                         placeholder="<?php esc_attr_e('Password', 'mos'); ?>"/>
-              </div>
-
-              <div class="form-footer">
-                  <div class="form-footer-meta">
-                    <?php esc_html_e('Don\'t have an account yet?', 'mos'); ?>
-                      <a class="animated-link"
-                         href="<?php echo esc_url(site_url('/utilizatori/inregistrare')); ?>"><?php esc_html_e('Register', 'mos'); ?></a>
-                  </div>
-                  <button class="form-subscribe-button"
-                          type="submit"><?php esc_html_e('Log in to your account', 'mos'); ?></button>
-              </div>
-
-          </form>
-          <!-- eof: login form -->
-      </div>
-
-    <?php
-    $output = ob_get_contents();
-    ob_end_clean();
-
-    return $output;
-  }
--- includes/blocks/register-form.php
+++ includes/blocks/register-form.php
@@ -1,112 +1,165 @@
 <?php
-  function mos_register_form()
-  {
-    if (is_user_logged_in()) {
-      return '';
-    }
-
-    // Process form submission
-    if (isset($_POST['register-form-submit'])) {
-      $name = sanitize_text_field($_POST['register-form-name']);
-      $surname = sanitize_text_field($_POST['register-form-surname']);
-      $email = sanitize_email($_POST['register-form-email']);
-      $password = sanitize_text_field($_POST['register-form-password']);
-      $confirm_password = sanitize_text_field($_POST['register-form-confirm-password']);
-
-      // Perform validation on form data
-      $errors = array();
-
-      if (empty($name)) {
-        $errors[] = __('First Name is required', 'mos');
-      }
-
-      if (empty($surname)) {
-        $errors[] = __('Last Name is required', 'mos');
-      }
-
-      if (empty($email)) {
-        $errors[] = __('Email is required', 'mos');
-      } elseif (!is_email($email)) {
-        $errors[] = __('Email is invalid', 'mos');
-      } elseif (email_exists($email)) {
-        $errors[] = __('This email is already registered', 'mos');
-      }
-
-      if (empty($password)) {
-        $errors[] = __('Password is required', 'mos');
-      } elseif ($password != $confirm_password) {
-        $errors[] = __('Passwords do tot match', 'mos');
-      }
-
-      // If there are errors, display them
-      if (!empty($errors)) {
-        echo '<div class="error">' . implode('<br>', $errors) . '</div>';
-      } else {
-        // Create new user
-        $user_id = wp_create_user($email, $password, $email);
-
-        if (is_wp_error($user_id)) {
-          echo '<div class="error">' . $user_id->get_error_message() . '</div>';
-        } else {
-          // Set user meta data
-          update_user_meta($user_id, 'first_name', $name);
-          update_user_meta($user_id, 'last_name', $surname);
-
-          // Log user in
-          wp_set_auth_cookie($user_id, true);
-          wp_redirect(home_url());
-          exit;
+    function mos_register_form() {
+        if ( is_user_logged_in() ) {
+            return '';
         }
-      }
+
+        // Perform validation on form data
+        $errors = array();
+
+        // Process form submission
+        if ( isset( $_POST['register-form-submit'] ) ) {
+            $name             = sanitize_text_field( $_POST['register-form-name'] );
+            $surname          = sanitize_text_field( $_POST['register-form-surname'] );
+            $email            = sanitize_email( $_POST['register-form-email'] );
+            $password         = sanitize_text_field( $_POST['register-form-password'] );
+            $confirm_password = sanitize_text_field( $_POST['register-form-confirm-password'] );
+
+
+            if ( empty( $name ) ) {
+                $errors[] = __( 'First Name is required', 'mos' );
+            }
+
+            if ( empty( $surname ) ) {
+                $errors[] = __( 'Last Name is required', 'mos' );
+            }
+
+            if ( empty( $email ) ) {
+                $errors[] = __( 'Email is required', 'mos' );
+            } elseif ( ! is_email( $email ) ) {
+                $errors[] = __( 'Email is invalid', 'mos' );
+            } elseif ( email_exists( $email ) ) {
+                $errors[] = __( 'This email is already registered', 'mos' );
+            }
+
+            if ( empty( $password ) ) {
+                $errors[] = __( 'Password is required', 'mos' );
+            } elseif ( $password != $confirm_password ) {
+                $errors[] = __( 'Passwords do tot match', 'mos' );
+            }
+
+            // If there are errors, display them
+            if ( empty( $errors ) ) {
+                // Create new user
+                $user_id = wp_create_user( $email, $password, $email );
+
+                if ( is_wp_error( $user_id ) ) {
+                    echo '<div class="error">' . $user_id->get_error_message() . '</div>';
+                } else {
+                    // Set user meta data
+                    update_user_meta( $user_id, 'first_name', $name );
+                    update_user_meta( $user_id, 'last_name', $surname );
+
+                    // Log user in
+                    wp_set_auth_cookie( $user_id, true );
+                    wp_redirect( home_url() );
+                    exit;
+                }
+            }
+        }
+
+        // Display registration form
+        ob_start(); ?>
+
+        <div class="wp-block-mos-register-form">
+            <!-- sof: register form -->
+            <form id="register-form" class="register-form" method="post">
+
+                <div class="form-group">
+                    <label><?php esc_html_e( 'First Name', 'mos' ); ?></label>
+                    <input type="text" id="register-form-name" name="register-form-name" class="form-input"
+                           placeholder="<?php esc_attr_e( 'First Name', 'mos' ) ?>"/>
+                    <div id="login-status" class="login-status">
+                        <?php
+                            if ( isset( $_POST['register-form-submit'] ) ) {
+                                if ( empty( $_POST['register-form-name'] ) ) {
+                                    echo '<div class="error">First Name is required</div>';
+                                }
+                            }
+                        ?>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label><?php esc_html_e( 'Last Name', 'mos' ); ?></label>
+                    <input type="text" id="register-form-surname" name="register-form-surname" class="form-input"
+                           placeholder="<?php esc_attr_e( 'Last Name', 'mos' ) ?>"/>
+                    <div id="login-status" class="login-status">
+                        <?php
+                            if ( isset( $_POST['register-form-submit'] ) ) {
+                                if ( empty( $_POST['register-form-surname'] ) ) {
+                                    echo '<div class="error">Last Name is required</div>';
+                                }
+                            }
+                        ?>
+                    </div>
+                </div>
+
+
+                <div class="form-group">
+                    <label><?php esc_html_e( 'Email', 'mos' ); ?></label>
+                    <input type="email" id="register-form-email" name="register-form-email" class="form-input"
+                           placeholder="<?php esc_attr_e( 'Email', 'mos' ); ?>"/>
+                    <div id="login-status" class="login-status">
+                        <?php
+                            if ( isset( $_POST['register-form-submit'] ) ) {
+                                if ( empty( $_POST['register-form-email'] ) ) {
+                                    echo '<div class="error">Email is required</div>';
+                                } elseif ( ! is_email( $_POST['register-form-email'] ) ) {
+                                    echo '<div class="error">Email is invalid</div>';
+                                } elseif ( email_exists( $_POST['register-form-email'] ) ) {
+                                    echo '<div class="error">This email is already registered</div>';
+                                }
+                            }
+                        ?>
+                    </div>
+                </div>
+
+
+                <div class="form-group">
+                    <label><?php esc_html_e( 'Password', 'mos' ); ?></label>
+                    <input type="password" id="register-form-password" name="register-form-password" class="form-input"
+                           placeholder="<?php esc_attr_e( 'Password', 'mos' ); ?>"/>
+                    <div id="login-status" class="login-status">
+                        <?php
+                            if ( isset( $_POST['register-form-submit'] ) ) {
+                                if ( empty( $_POST['register-form-password'] ) ) {
+                                    echo '<div class="error">Password is required</div>';
+                                }
+                            }
+                        ?>
+                    </div>
+                </div>
+
+
+                <div class="form-group">
+                    <label><?php esc_html_e( 'Repeat Password', 'mos' ); ?></label>
+                    <input type="password" id="register-form-confirm-password" name="register-form-confirm-password"
+                           class="form-input"
+                           placeholder="<?php esc_attr_e( 'Repeat Password', 'mos' ); ?>"/>
+                    <div id="login-status" class="login-status">
+                        <?php
+                            if ( isset( $_POST['register-form-submit'] ) ) {
+                                if ( empty( $_POST['register-form-confirm-password'] ) ) {
+                                    echo '<div class="error">Repeat Password is required</div>';
+                                } elseif ( $_POST['register-form-password'] != $_POST['register-form-confirm-password'] ) {
+                                    echo '<div class="error">Passwords do not match</div>';
+                                }
+                            }
+                        ?>
+                    </div>
+                </div>
+
+
+                <button type="submit" class="form-subscribe-button"
+                        name="register-form-submit"><?php esc_html_e( 'Create Account', 'mos' ); ?></button>
+            </form>
+            <!-- eof: register form -->
+        </div>
+
+        <?php
+        $output = ob_get_contents();
+        ob_end_clean();
+
+        return $output;
     }
-
-    // Display registration form
-    ob_start(); ?>
-
-      <div class="wp-block-mos-register-form">
-          <!-- sof: register form -->
-          <form id="register-form" class="register-form" method="post">
-
-              <div class="form-group">
-                  <label><?php esc_html_e('First Name', 'mos'); ?></label>
-                  <input type="text" id="register-form-name" name="register-form-name" class="form-input"
-                         placeholder="<?php esc_attr_e('First Name', 'mos') ?>"/>
-              </div>
-
-              <div class="form-group">
-                  <label><?php esc_html_e('Last Name', 'mos'); ?></label>
-                  <input type="text" id="register-form-surname" name="register-form-surname" class="form-input"
-                         placeholder="<?php esc_attr_e('Last Name', 'mos') ?>"/>
-              </div>
-
-              <div class="form-group">
-                  <label><?php esc_html_e('Email', 'mos'); ?></label>
-                  <input type="email" id="register-form-email" name="register-form-email" class="form-input"
-                         placeholder="<?php esc_attr_e('Email', 'mos'); ?>"/>
-              </div>
-
-              <div class="form-group">
-                  <label><?php esc_html_e('Password', 'mos'); ?></label>
-                  <input type="password" id="register-form-password" name="register-form-password" class="form-input"
-                         placeholder="<?php esc_attr_e('Password', 'mos'); ?>"/>
-              </div>
-
-              <div class="form-group">
-                  <label><?php esc_html_e('Repeat Password', 'mos'); ?></label>
-                  <input type="password" id="register-form-confirm-password" name="register-form-confirm-password"
-                         class="form-input"
-                         placeholder="<?php esc_attr_e('Repeat Password', 'mos'); ?>"/>
-              </div>
-
-              <button type="submit" class="form-subscribe-button"
-                      name="register-form-submit"><?php esc_html_e('Create Account', 'mos'); ?></button>
-          </form>
-          <!-- eof: register form -->
-      </div>
-
-    <?php
-    $output = ob_get_contents();
-    ob_end_clean();
-
-    return $output;
-  }
